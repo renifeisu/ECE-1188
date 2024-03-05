5d724a8c944b5e35608e67a74a1e604b1a07e7a1
aji11PITT <97813049+aji11PITT@users.noreply.github.com>
2024-03-05 08:20:08 -0500

    Add files via upload

--- FSM Files/Reflectance.c
+++ FSM Files/Reflectance.c
@@ -1,67 +1,7 @@
-// Reflectance.c
-// Provide functions to take measurements using the kit's built-in
-// QTRX reflectance sensor array.  Pololu part number 3672. This works by outputting to the
-// sensor, waiting, then reading the digital value of each of the
-// eight phototransistors.  The more reflective the target surface is,
-// the faster the voltage decays.
-// Daniel and Jonathan Valvano
-// July 11, 2019
-
-/* This example accompanies the book
-   "Embedded Systems: Introduction to Robotics,
-   Jonathan W. Valvano, ISBN: 9781074544300, copyright (c) 2019
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
-
-Simplified BSD License (FreeBSD License)
-Copyright (c) 2019, Jonathan Valvano, All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are
-those of the authors and should not be interpreted as representing official
-policies, either expressed or implied, of the FreeBSD Project.
-*/
-
-// reflectance even LED illuminate connected to P5.3
-// reflectance odd LED illuminate connected to P9.2
-// reflectance sensor 1 connected to P7.0 (robot's right, robot off road to left)
-// reflectance sensor 2 connected to P7.1
-// reflectance sensor 3 connected to P7.2
-// reflectance sensor 4 connected to P7.3 center
-// reflectance sensor 5 connected to P7.4 center
-// reflectance sensor 6 connected to P7.5
-// reflectance sensor 7 connected to P7.6
-// reflectance sensor 8 connected to P7.7 (robot's left, robot off road to right)
-
 #include <stdint.h>
 #include "msp432.h"
 #include "..\inc\Clock.h"
 
-// ------------Reflectance_Init------------
-// Initialize the GPIO pins associated with the QTR-8RC
-// reflectance sensor.  Infrared illumination LEDs are
-// initially off.
-// Input: none
-// Output: none
 void Reflectance_Init(void){
 
     // CTRL EVEN -> Pin 5.3
@@ -82,17 +22,7 @@ void Reflectance_Init(void){
     P7->DIR &= ~0x00;     // 7.0-7.7 as INPUT
 }
 
-// ------------Reflectance_Read------------
-// Read the eight sensors
-// Turn on the 8 IR LEDs
-// Pulse the 8 sensors high for 10 us
-// Make the sensor pins input
-// wait t us
-// Read sensors
-// Turn off the 8 IR LEDs
-// Input: time to wait in usec
-// Output: sensor readings
-// Assumes: Reflectance_Init() has been called
+
 uint8_t Reflectance_Read(uint32_t time){
 
     uint8_t result;
@@ -114,6 +44,27 @@ uint8_t Reflectance_Read(uint32_t time){
     P9->OUT &= ~0x04;     // turn off the 4 odd  IR LEDs
 
     //Need to make output 5 bits instead of 8 bit
+    result = ~result;
+    uint8_t temp = result;
+    temp = temp >> 1;
+    temp &= 0x2A;
+    result |= temp; ////////
+    temp = result;
+    temp = temp >> 1;
+    temp |= 0xFB;
+    result |= 0x04;
+    result &= temp; ////////
+    temp = result;
+    temp = temp >> 2;
+    temp |= 0xF7;
+    result |= 0x08;
+    result &= temp; ////////
+    temp = result;
+    temp = temp >> 3;
+    temp |= 0xEF;
+    result |= 0x10;
+    result &= temp; ////////
+    result &= 0x1F;
 
     return result;        // return the 8-bit value that represents the state of the sensors
 }
